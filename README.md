073dea57866628469a1da06265c25fe6d93930b7
assadali22 <alia2832@outlook.com>
2026-04-23 16:27:16 -0700

    React and NextJS setup with npx create next app. Also added a home tsx page to test the starter home page.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # inf-124-overwatch-quiz
+
+React + Next.js Project for an Overwatch Quiz Game.
---
+++ overwatch-quiz-web/src/app/Home.tsx
@@ -0,0 +1,8 @@
+import Home from "./page"; /* import Home() function from page.tsx, which has the html markup */
+
+export default function HomePage()
+{
+    return (
+        <Home></Home>
+    )
+}
