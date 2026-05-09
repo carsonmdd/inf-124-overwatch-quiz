16c6427592d000fc8559a1769f09d6bf3d487d7a
Nathan McLaughlin <namclaug@uci.edu>
2026-05-08 20:41:06 -0700

    Added the about page, may change the logo in the future.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-	return (
-		<div className="flex-1 flex items-center justify-center">
-			<h1 className="text-4xl font-black uppercase italic">About</h1>
-		</div>
-	);
+  return (
+    <div className="min-h-screen p-3 bg-ow-dark-blue text-white rounded-sm shadow-lg">
+      <div className="flex-1 flex items-top justify-center pt-3 pb-6">
+        <h1 className="text-2xl font-black uppercase underline decoration-4">About The Team</h1>
+      </div>
+
+      <div className="flex flex-col md:flex-row pb-10">
+        <div className="flex-2 flex items-center justify-start pl-5 tracking-wider">
+          <p className="font-semibold text-lg leading-relaxed">
+            We are a team of five undergraduate students who wanted to make a
+            website which would allow users to test their knowledge of
+            Overwatch. This project was intialized, planned, and executed within
+            a ten week period, primarily using the React library. If you have
+            any questions or comments, please contact us with the information
+            given below.
+          </p>
+        </div>
+        <div className="flex-1 flex items-center justify-end pr-4 pb-10">
+          <img src="https://www.freepnglogos.com/uploads/overwatch---------------------------tokyo--30.png" />
+        </div>
+      </div>
+      <div className="flex flex-row md:flex-col bg-gray-900 pt-3 pb-6 rounded-sm items-center justify-center">
+        <div className="flex flex-1 items-center justify-center pb-3">
+          <h2 className="font-black text-s text-orange-300">
+            Contact Information
+          </h2>
+        </div>
+        <p className="font-black text-xs">
+          aassad1@uci.edu • carsonmd@uci.edu • jasonbm2@uci.edu •
+          namclaug@uci.edu • tapiarr@uci.edu
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
